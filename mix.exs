eac403d554e701c64e7a4d05d60299b0bbc1f316
Tyler Clemens <tyler.r.clemens@gmail.com>
2026-02-24 04:28:19 -0700

    Ready for publish

--- mix.exs
+++ mix.exs
@@ -7,22 +7,28 @@ defmodule ExPidController.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A discrete PID controller for feedback control loops in Elixir, useful for robotics, automation, and simulation",
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tyler/ex_pid_controller"}
+    ]
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
     ]
   end
 end
